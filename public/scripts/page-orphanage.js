7c1fca972172d2a9851ce5b918629f4603ab87f1
Rafael Paiva <raffaelgustavo@hotmail.com>
2020-10-16 17:54:09 -0300

    Sqlite adicionado e configurado e interpolação com template engine nas paginas puxando informações da database

--- public/scripts/page-orphanage.js
+++ public/scripts/page-orphanage.js
@@ -6,8 +6,12 @@ const options = {
   zoomControl: false,
 };
 
+//get values from html
+const lat = document.querySelector('span[data-lat]').dataset.lat
+const lng = document.querySelector('span[data-lng]').dataset.lng
+
 //create map
-const map = L.map("mapid", options).setView([-8.0612817, -34.8926457], 15);
+const map = L.map("mapid", options).setView([lat, lng], 15);
 
 //create and add tileLayer
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
@@ -21,7 +25,8 @@ const icon = L.icon({
 });
 
 //create and add maker
-L.marker([-8.0612817, -34.8926457], { icon }).addTo(map);
+
+L.marker([lat, lng], { icon }).addTo(map);
 
 /* image gallery */
 
--- src/pages.js
+++ src/pages.js
@@ -1,19 +1,50 @@
-const orphanages = require('./database/fakedata.js');
+const Database = require('./database/db');
 
 module.exports = {
   index(req, res) {
     return res.render("index");
   },
 
-  orphanage(req, res) {
-    return res.render("orphanage");
+ async orphanage(req, res) {
+
+  const id = req.query.id
+
+    try {
+      const db = await Database;
+      const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
+      const orphanage = results[0]
+
+      /* if ternario javascript */
+      orphanage.open_on_weekends = orphanage.open_on_weekends == 1 ? true : false; 
+      /*quebra o array de imagens */
+      orphanage.images = orphanage.images.split(",")
+      orphanage.firstImage = orphanage.images[0]
+
+      return res.render("orphanage" , { orphanage })
+
+    } catch (error) {
+      console.log(erro)
+      return res.send('Erro no banco de dados!')
+    } 
   },
 
-  orphanages(req, res) {
-    return res.render("orphanages", { orphanages });
+  async orphanages(req, res) {
+    try {
+        const db = await Database;
+        const orphanages = await db.all("SELECT * FROM orphanages")
+        return res.render("orphanages", { orphanages });
+      
+    } catch (error) {
+      console.log(error)
+      return res.send("Erro no banco de dados!")
+    }
   },
 
   createOrphanage(req, res) {
     return res.render("create-orphanage");
   },
+
+  saveOrphanage(req, res) {
+      
+  }
 };
