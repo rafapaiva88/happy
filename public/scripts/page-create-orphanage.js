7b91f185f95774df397b760669b9ac3280c23066
Rafael Paiva <raffaelgustavo@hotmail.com>
2020-10-16 22:54:24 -0300

    finalizando o projeto inicial com a integração da database na pagina de criação do orfanato.

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -8,82 +8,91 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 const icon = L.icon({
   iconUrl: "/images/map-marker.svg",
   iconSize: [58, 68],
-  iconAnchor: [29, 68]
+  iconAnchor: [29, 68],
 });
 
 let marker;
 
 //create and add maker
 
-map.on('click', (event) => {
-    const lat = event.latlng.lat;
-    const lng = event.latlng.lng;
+map.on("click", (event) => {
+  const lat = event.latlng.lat;
+  const lng = event.latlng.lng;
 
-    document.querySelector('[name=lat]').value = lat;
-    document.querySelector('[name=lng]').value = lng;
+  document.querySelector("[name=lat]").value = lat;
+  document.querySelector("[name=lng]").value = lng;
 
-    // remove icon
+  // remove icon
 
-    marker && map.removeLayer(marker);
+  marker && map.removeLayer(marker);
 
-    // add icon layer
+  // add icon layer
 
-    marker = L.marker([lat, lng], { icon }).addTo(map);
-})
+  marker = L.marker([lat, lng], { icon }).addTo(map);
+});
 
 // adicionar campo de fotos
 
 function addPhotoField() {
-    
-    // pegar o container de #images
-    const container = document.querySelector('#images');
+  // pegar o container de #images
+  const container = document.querySelector("#images");
 
-    // pegar o container para duplicar .new-upload
-    const fieldsContainer = document.querySelectorAll('.new-upload');
+  // pegar o container para duplicar .new-upload
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    // realizar o clone do ultima caixa de upload adicionada
-    const newFieldContainer = fieldsContainer[fieldsContainer.length - 1].cloneNode(true);
+  // realizar o clone do ultima caixa de upload adicionada
+  const newFieldContainer = fieldsContainer[
+    fieldsContainer.length - 1
+  ].cloneNode(true);
 
-    // verifica se o campo está vazio, se estiver não adiciona o container
-    if (newFieldContainer.children[0].value == ""){
-        return; 
-    }
-    // limpar conteudo escrito no campo antes de adicionar ao container #images
-    newFieldContainer.children[0].value = "";
+  // verifica se o campo está vazio, se estiver não adiciona o container
+  if (newFieldContainer.children[0].value == "") {
+    return;
+  }
+  // limpar conteudo escrito no campo antes de adicionar ao container #images
+  newFieldContainer.children[0].value = "";
 
-    //adicionar o clone ao container #images
-    container.appendChild(newFieldContainer);
-    
-};
+  //adicionar o clone ao container #images
+  container.appendChild(newFieldContainer);
+}
 
 function deleteField(event) {
-    const span = event.currentTarget;
+  const span = event.currentTarget;
 
-    const fieldsContainer = document.querySelectorAll('.new-upload');
+  const fieldsContainer = document.querySelectorAll(".new-upload");
 
-    if (fieldsContainer.length <= 1) {
+  if (fieldsContainer.length <= 1) {
     // limpar campo
     span.parentNode.children[0].value = "";
-        return;
-    }
-    // deletar campo
-    span.parentNode.remove();
+    return;
+  }
+  // deletar campo
+  span.parentNode.remove();
 }
 
 //select yer or no
 
-function toggleSelect(event){
-    
-    //retirar a class .active dos botões
-    document.querySelectorAll('.button-select button')
-    .forEach( button => button.classList.remove('active'))
+function toggleSelect(event) {
+  //retirar a class .active dos botões
+  document
+    .querySelectorAll(".button-select button")
+    .forEach((button) => button.classList.remove("active"));
 
-    // colocar o active no botao clicado
-    const button = event.currentTarget
-    button.classList.add('active')
+  // colocar o active no botao clicado
+  const button = event.currentTarget;
+  button.classList.add("active");
 
-    const input = document.querySelector('[name="open_on_weekends"]')
+  const input = document.querySelector('[name="open_on_weekends"]');
 
-    input.value = button.dataset.value;
+  input.value = button.dataset.value;
 }
 
+// valida se o mapa está sendo selecionado no formulario
+function validate(event) {
+  const mapMark = document.querySelector('[name="lat"]');
+
+  if (mapMark.value == "") {
+    event.preventDefault();
+    alert("Selecione um ponto no mapa");
+  }
+}
--- src/pages.js
+++ src/pages.js
@@ -1,42 +1,43 @@
-const Database = require('./database/db');
+const Database = require("./database/db");
+const saveOrphanage = require("./database/saveOrphanage");
 
 module.exports = {
   index(req, res) {
     return res.render("index");
   },
 
- async orphanage(req, res) {
-
-  const id = req.query.id
+  async orphanage(req, res) {
+    const id = req.query.id;
 
     try {
       const db = await Database;
-      const results = await db.all(`SELECT * FROM orphanages WHERE id = "${id}"`)
-      const orphanage = results[0]
+      const results = await db.all(
+        `SELECT * FROM orphanages WHERE id = "${id}"`
+      );
+      const orphanage = results[0];
 
       /* if ternario javascript */
-      orphanage.open_on_weekends = orphanage.open_on_weekends == 1 ? true : false; 
+      orphanage.open_on_weekends =
+        orphanage.open_on_weekends == 1 ? true : false;
       /*quebra o array de imagens */
-      orphanage.images = orphanage.images.split(",")
-      orphanage.firstImage = orphanage.images[0]
+      orphanage.images = orphanage.images.split(",");
+      orphanage.firstImage = orphanage.images[0];
 
-      return res.render("orphanage" , { orphanage })
-
+      return res.render("orphanage", { orphanage });
     } catch (error) {
-      console.log(erro)
-      return res.send('Erro no banco de dados!')
-    } 
+      console.log(erro);
+      return res.send("Erro no banco de dados!");
+    }
   },
 
   async orphanages(req, res) {
     try {
-        const db = await Database;
-        const orphanages = await db.all("SELECT * FROM orphanages")
-        return res.render("orphanages", { orphanages });
-      
+      const db = await Database;
+      const orphanages = await db.all("SELECT * FROM orphanages");
+      return res.render("orphanages", { orphanages });
     } catch (error) {
-      console.log(error)
-      return res.send("Erro no banco de dados!")
+      console.log(error);
+      return res.send("Erro no banco de dados!");
     }
   },
 
@@ -44,7 +45,28 @@ module.exports = {
     return res.render("create-orphanage");
   },
 
-  saveOrphanage(req, res) {
-      
-  }
+  async saveOrphanage(req, res) {
+    const fields = req.body;
+
+    try {
+      //salvar um orfanato
+      const db = await Database;
+      await saveOrphanage(db, {
+        lat: fields.lat,
+        lng: fields.lng,
+        name: fields.name,
+        about: fields.about,
+        whatsapp: fields.whatsapp,
+        images: fields.images.toString(),
+        instructions: fields.instructions,
+        opening_hours: fields.opening_hours,
+        open_on_weekends: fields.open_on_weekends,
+      });
+      //redirecionar
+      return res.redirect("/orphanages");
+    } catch (error) {
+      console.log(error);
+      return res.send("Erro! Impossível salvar no banco de dados");
+    }
+  },
 };
